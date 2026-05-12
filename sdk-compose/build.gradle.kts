255415b483c96f92dbf033565f25393e13aaeb8e
elysiak <yevhenii.lysiak@audienzz.com>
2026-05-12 13:10:09 +0300

    Sonatype release updates.

--- sdk-compose/build.gradle.kts
+++ sdk-compose/build.gradle.kts
@@ -1,12 +1,18 @@
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.maven.publish)
+    `signing`
 }
 
 android {
     namespace = "org.audienzz.bta.compose"
     compileSdk = 36
+    version = "0.1.0"
 
     defaultConfig {
         minSdk = 24
@@ -35,4 +41,39 @@ dependencies {
     // Expose lifecycle-runtime-compose transitively so publishers using BtaFeed
     // don't need to add it themselves (provides LocalLifecycleOwner, etc.).
     api(libs.androidx.lifecycle.runtime.compose)
+}
+
+signing {
+    useGpgCmd()
+}
+
+mavenPublishing {
+    configure(AndroidSingleVariantLibrary())
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+    signAllPublications()
+    coordinates("com.audienzz", "bta-sdk-compose", version.toString())
+    pom {
+        name.set("BtaAudienzz Compose")
+        description.set("Jetpack Compose wrapper for the Audienzz BTA feed SDK")
+        inceptionYear.set("2026")
+        url.set("https://github.com/audienzz/bta-audienzz-android-sdk/blob/main/README.md")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("tech@audienzz.ch")
+                name.set("Audienzz")
+                url.set("https://github.com/audienzz")
+            }
+        }
+        scm {
+            connection.set("scm:git:git://github.com/audienzz/bta-audienzz-android-sdk.git")
+            developerConnection.set("scm:git:ssh://github.com/audienzz/bta-audienzz-android-sdk.git")
+            url.set("https://github.com/audienzz/bta-audienzz-android-sdk")
+        }
+    }
 }
--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -1,11 +1,17 @@
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.maven.publish)
+    `signing`
 }
 
 android {
     namespace = "org.audienzz.bta.sdk"
     compileSdk = 36
+    version = "0.1.0"
 
     defaultConfig {
         minSdk = 24
@@ -31,4 +37,39 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+}
+
+signing {
+    useGpgCmd()
+}
+
+mavenPublishing {
+    configure(AndroidSingleVariantLibrary())
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+    signAllPublications()
+    coordinates("com.audienzz", "bta-sdk", version.toString())
+    pom {
+        name.set("BtaAudienzz")
+        description.set("Android BTA (Below The Article) feed SDK by Audienzz")
+        inceptionYear.set("2026")
+        url.set("https://github.com/audienzz/bta-audienzz-android-sdk/blob/main/README.md")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("tech@audienzz.ch")
+                name.set("Audienzz")
+                url.set("https://github.com/audienzz")
+            }
+        }
+        scm {
+            connection.set("scm:git:git://github.com/audienzz/bta-audienzz-android-sdk.git")
+            developerConnection.set("scm:git:ssh://github.com/audienzz/bta-audienzz-android-sdk.git")
+            url.set("https://github.com/audienzz/bta-audienzz-android-sdk")
+        }
+    }
 }
